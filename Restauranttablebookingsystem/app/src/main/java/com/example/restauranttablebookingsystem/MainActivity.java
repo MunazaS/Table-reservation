abb6752fc9df89c1d3c6d337d065f16e3807f3f5
Munaza <munazasaghir4@gmail.com>
2023-11-14 19:30:30 +0500

    first commit

---
+++ Restauranttablebookingsystem/app/src/main/java/com/example/restauranttablebookingsystem/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.restauranttablebookingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Looper;
+
+class splashScreen extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash_screen)
+//        Handler(Looper.getMainLooper()).postDelayed({
+//                startActivity(Intent(packageContent:this,MainActivity::class.java))
+//        finish()
+//        } ,delaymillis:3000)
+    }
+}
